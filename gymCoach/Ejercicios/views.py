f3157a264fe1cb8a00070ce9c50419d3d8ab8ddd
Romy Wakabayashi <romywakabayashi@gmail.com>
2023-12-23 22:22:08 -0500

    avance

--- gymCoach/Ejercicios/views.py
+++ gymCoach/Ejercicios/views.py
@@ -3,36 +3,50 @@ from django.http import HttpResponse
 from Ejercicios.models import Ejercicio
 from django.views.generic import ListView,CreateView,UpdateView,DetailView,DeleteView
 from django.urls import reverse_lazy
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+# mixin class with login required decorator 
+
+class LoginRequiredMixin:
+    @method_decorator(login_required)
+    def dispatch(self, *args,**kwargs):
+        return super().dispatch(*args,**kwargs)
+    
+# Base class for views with login_required
+class LoginRequiredView(LoginRequiredMixin):
+    pass
+
+
 
 #READ
-class EjercicioListview(ListView):
+class EjercicioListview(LoginRequiredView,ListView):
     model= Ejercicio
     context_object_name= "ejercicios"
     template_name= "Ejercicios/ejercicio_lista.html"
 
 
 #DETAIL
-class EjercicioDetailView(DetailView):
+class EjercicioDetailView(LoginRequiredView,DetailView):
     model=Ejercicio    
     template_name="Ejercicios/ejercicio_detalle.html"
 
 # CREATE
-class EjercicioCreateview(CreateView):
+class EjercicioCreateview(LoginRequiredView,CreateView):
     model= Ejercicio
     template_name= "Ejercicios/ejercicio_crear.html"
     success_url = reverse_lazy('ejercicios lista')
     fields=['nombre','descripcion', 'grupo_muscular']
 
 #UPDATE
-class EjercicioUpdateView(UpdateView):
+class EjercicioUpdateView(LoginRequiredView,UpdateView):
     model= Ejercicio
     template_name= "Ejercicios/ejercicio_editar.html"
     success_url = reverse_lazy('ejercicios lista')
     fields=['nombre','descripcion','grupo_muscular']
 
 #DELETE
-class EjercicioDeleteView(DeleteView):
+class EjercicioDeleteView(LoginRequiredView,DeleteView):
     model= Ejercicio
     template_name= "Ejercicios/ejercicio_eliminar.html"
     success_url = reverse_lazy('ejercicios lista')
--- gymCoach/gymCoach/settings.py
+++ gymCoach/gymCoach/settings.py
@@ -133,3 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#login url 
+LOGIN_URL = 'login'  
